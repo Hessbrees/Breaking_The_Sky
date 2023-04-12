3b8f57fd3f8e71f9b7e78d3715de63c08129c0a8
Hessbrees <lenart483@gmail.com>
2023-04-12 18:10:30 +0200

    Player base

---
+++ Assets/Scripts/Map/PlayerStructures/PlayerStructures.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStructures : MonoBehaviour
+{
+    public List<Vector2> structuresTiles;
+
+    private BoxCollider2D saveZone;
+
+
+}
